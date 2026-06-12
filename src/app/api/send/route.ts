5fab5853d6f0d9785c39a774b1f2d0c04e30c258
otochi <charlycuello123@gmail.com>
2026-06-12 20:44:58 -0300

    Refactor email sending logic in API route for improved clarity and error handling
    - Updated the email sending logic in the POST request handler to enhance readability and maintainability.
    - Changed hardcoded email addresses to dynamic values for better configurability.
    - Improved error handling and response formatting for better API usability.

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -1,32 +1,38 @@
-import { NextRequest, NextResponse } from 'next/server';
-import { Resend } from 'resend';
+import { NextRequest, NextResponse } from "next/server";
+import { Resend } from "resend";
 
 export async function POST(req: NextRequest) {
-    const { name, lastName, phone, message } = await req.json();
-    const token = process.env.RESEND_TOKEN;
+  const { name, lastName, phone, message } = await req.json();
+  const token = process.env.RESEND_TOKEN;
 
-    if (!token) {
-        return NextResponse.json({ error: 'Email service is not configured' }, { status: 500 });
-    }
+  if (!token) {
+    return NextResponse.json(
+      { error: "Email service is not configured" },
+      { status: 500 },
+    );
+  }
 
-    const resend = new Resend(token);
+  const resend = new Resend(token);
 
-    try {
-        await resend.emails.send({
-            from: 'Portfolio Ana <onboarding@resend.dev>',
-            to: 'charlycuello123@gmail.com',
-            subject: 'Portfolio Ana - New message from contact form',
-            text: `
+  try {
+    await resend.emails.send({
+      from: "Portfolio Ana <anaartista.com>",
+      to: "anaartista122@gmail.com",
+      subject: "Portfolio Ana - New message from contact form",
+      text: `
                 Name: ${name}
                 Last Name: ${lastName}
                 Phone: ${phone}
                 
                 Message: ${message}
-            `
-        });
-        return NextResponse.json({ message: 'Form submitted successfully' }, { status: 200 });
-    } catch (error) {
-        console.error('Error sending form:', error);
-        return NextResponse.json({ error: 'Error sending form' }, { status: 500 });
-    }
+            `,
+    });
+    return NextResponse.json(
+      { message: "Form submitted successfully" },
+      { status: 200 },
+    );
+  } catch (error) {
+    console.error("Error sending form:", error);
+    return NextResponse.json({ error: "Error sending form" }, { status: 500 });
+  }
 }
